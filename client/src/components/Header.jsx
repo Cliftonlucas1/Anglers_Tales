5fb73884ad8f4b11dfb8eca04ce188150db7cb86
Clifton Lucas <cliftonlucas89@gmail.com>
2023-04-04 10:04:02 -0500

    merge

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import ReactSwitch from 'react-switch'
 
+const Switch = ({ isChecked, handleSwitchChange }) => {
+  return (
+    <ReactSwitch checked={isChecked} onChange={handleSwitchChange} />
+  )
+}
+
 const Header = ({ user, handleLogOut, toggleTheme }) => {
-  let userOptions
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleSwitchChange = () => {
+    setIsChecked(!isChecked)
+    toggleTheme()
+  }
+
+  let userOptions;
   if (user) {
     userOptions = (
       <div className="logoHomeAbout">
         <p className="welcomemessage">Welcome {user.userName}!</p>
         {/*  */}
+
+
 
 <div>
 
@@ -37,7 +52,7 @@ const Header = ({ user, handleLogOut, toggleTheme }) => {
         {/* <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a> */}
       </li>
       <li>
-      <ReactSwitch onChange={toggleTheme} />
+ <Switch isChecked={isChecked} handleSwitchChange={handleSwitchChange} />
       </li>
     </ul>
   </div>
