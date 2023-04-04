aacaf88fd3d4bdc08581027ccfb25d48e43bc68e
Clifton Lucas <cliftonlucas89@gmail.com>
2023-04-03 19:51:37 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
   const [userPostList, setUserPostList] = useState([])
   //
   const [user, setUser] = useState(null)
-
+  //
   const [postList, setPostList] = useState([])
 
   const [comments, setComments] = useState([])
